e42e2874ea32ff16ab0c9ab8f05910f42754bc6a
kikedB <edelbosque@e-sant.com>
2017-01-19 11:50:09 -0600

    perf-test

---
+++ typings/tsd.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="react-redux/react-redux.d.ts" />
+/// <reference path="react/react-dom.d.ts" />
+/// <reference path="react/react.d.ts" />
+/// <reference path="redux/redux.d.ts" />
+/// <reference path="react-addons-perf/index.d.ts" />
